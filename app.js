1dc4b51d402fb2bb9cf387fb5709a426d68199b2
sepehr <sepehrdesdgh@gmail.com>
2023-09-13 23:31:53 +0430

    learning index

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cors(corsOptions));
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP,please try again in an hour!',
+  message: 'Too many requests from this dD IP,please try again in an hour!',
 });
 
 app.use('/api', limiter);
